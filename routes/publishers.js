71fdb8a707530d6607c14eac461ab8ac0d391ade
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-06-07 12:38:19 +0600

    added publisher delete route

--- routes/publishers.js
+++ routes/publishers.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { verifyToken, verifyAdmin } from "../middlewares/auth.js";
 import { publisherCollection } from "../db.js";
-
+import { ObjectId } from "mongodb";
 const router = express.Router();
 
 // get publishers
@@ -20,5 +20,13 @@ router.post('/', verifyToken, verifyAdmin, async (req, res) => {
     const result = await publisherCollection.insertOne(publisher);
     res.send(result);
 });
+
+router.delete('/:id', async (req, res) => {
+    const publisher_id = req.params.id;
+    const query = { _id: new ObjectId(publisher_id) };
+    console.log(publisher_id);
+    const result = await publisherCollection.deleteOne(query);
+    res.send(result);
+})
 
 export default router;

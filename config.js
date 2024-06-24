df67482c0acf647d3247c23ded243d43691a60c9
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-06-24 18:23:20 +0600

    added vercel links in cors options

--- config.js
+++ config.js
@@ -2,5 +2,8 @@ export const corsOptions = [
     'http://localhost:5173',
     'http://localhost:5174',
     'https://nexus-news-nhb.web.app',
-    'https://nexus-news-nhb.firebaseapp.com'
+    'https://nexus-news-nhb.vercel.app',
+    'https://nexus-news-nhb.firebaseapp.com',
+    'https://nexus-news-nhb-nazmul-hassans-projects.vercel.app',
+    'https://nexus-news-nhb-nazmul-nhb-nazmul-hassans-projects.vercel.app'
 ];

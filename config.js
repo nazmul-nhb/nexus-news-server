44d0eb3592f557786a780befb81e43165295fe44
Nazmul Hassan <nazmulnhb007@yahoo.com>
2026-02-28 19:05:12 +0600

    updated CORS config

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 export const corsOptions = [
     'http://localhost:5173',
     'http://localhost:5174',
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'https://nazmul-nhb.dev',
+    'https://www.nazmul-nhb.dev',
+    'https://next.nazmul-nhb.dev',
+    'https://next-nhb.vercel.app',
     'https://nexus-news-nhb.web.app',
     'https://nexus-news-nhb.vercel.app',
     'https://nexus-news-nhb.firebaseapp.com',
